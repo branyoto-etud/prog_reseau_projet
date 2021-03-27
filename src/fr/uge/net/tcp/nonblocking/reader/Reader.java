3512db337ab43440e98f9762c50c91de808f3589
branyoto_etud <bgamei01@etud.u-pem.fr>
2021-03-27 17:47:01 +0100

    Doc

--- src/fr/uge/net/tcp/nonblocking/reader/Reader.java
+++ src/fr/uge/net/tcp/nonblocking/reader/Reader.java
@@ -2,6 +2,23 @@ package fr.uge.net.tcp.nonblocking.reader;
 
 import java.nio.ByteBuffer;
 
+/**
+ * <p>A reader is an object that take a buffer (in write-mode) as input and
+ * process it to obtain an element of type T.
+ * <p>
+ * The method {@link #process(ByteBuffer)} will return {@link ProcessStatus#REFILL} until
+ * the reader has finished reading data.
+ * If {@link #process(ByteBuffer)} encounter an error the method will return {@link ProcessStatus#ERROR}.
+ * The errors need to be explicitly specified in the implementations of {@link Reader}.
+ * <p>
+ * The method {@link #get()} can be used to obtain the processed element <b>only</b>
+ * if {@link #process(ByteBuffer)} return {@link ProcessStatus#DONE}. Otherwise the behaviour is undefined.
+ * Each call of the {@link #get()} method need to return the same value if no other method are called.
+ * <p>
+ * The method {@link #reset()} reset the {@link Reader} in it's initial state.
+ *
+ * @param <T> the type of the element this reader take care of.
+ */
 public interface Reader<T> {
     enum ProcessStatus {DONE,REFILL,ERROR}
 
